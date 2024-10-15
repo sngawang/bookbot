1f29762df480dfb49554227a5d907367ffb9c5d9
sngawang <s.ngawang@gmail.com>
2024-10-15 14:51:41 -0400

    add more functions on it

---
+++ main.py
@@ -0,0 +1,36 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} was found in the document")
+    for i in chars_dict.keys():
+        print(f"The '{i}' character was found {chars_dict[i]} times")
+    
+    print("--- End report ---")
+
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
